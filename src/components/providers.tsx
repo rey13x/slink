864193469c932e967787a2acd099fdfed9de37e4
rey13x <bagas139087@gmail.com>
2026-02-21 02:32:20 +0700

    Fix ThemeProvider property: forceTheme -> forcedTheme

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "~/components/theme-provider";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light" forceTheme="light" enableSystem={false}>
+    <ThemeProvider attribute="class" defaultTheme="light" forcedTheme="light" enableSystem={false}>
       <TooltipProvider>{children}</TooltipProvider>
     </ThemeProvider>
   );
